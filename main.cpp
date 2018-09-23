adbafe4aa9968c08284940825c7be2e40fd42d40
egorbarsukoff <egor.barsukoff@gmail.com>
2018-09-23 06:06:30 +0300

    basic launch code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "src/Point.h"
+#include "src/Gradient.h"
+#include "src/CommonRandom.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//    Rosenbrock f = Rosenbrock();
+//    auto a = Gradient<2>(f, Point<2>(std::array<double, 2>({0., 0.})));
+    std::cout << Point<2>(std::array<double, 2>({0., 0.}));
     return 0;
 }
