690a6f730202cd3f9c6b511f0c5754a2b30d2be9
egorbarsukoff <egor.barsukoff@gmail.com>
2019-10-09 17:42:37 +0300

    add square function

--- Functions.h
+++ Functions.h
@@ -8,21 +8,30 @@
 #include <Eigen/Core>
 #include "optim/Function.h"
 
-//! Функция Розенброка
+//! Функция Розенброка $[-2, 2]^2$
 class Rosenbrock : public AbstractFunction {
 
+protected:
     //! Вычиление функции в точке
     //! \param x Точка
     //! \return Результат
     [[nodiscard]] double compute(const Eigen::VectorXd &x) const override;
-protected:
     Eigen::VectorXd compute_grad(const Eigen::VectorXd &x) const override;
     Eigen::MatrixXd compute_hess(const Eigen::VectorXd &x) const override;
 
 public:
 
-    explicit Rosenbrock(BoxDomain domain);
+    explicit Rosenbrock();
 
+};
+
+//! $x^2 + 3y^2 + 6z^2 + 2xy $
+class SquareFunction : public AbstractFunction {
+
+protected:
+    [[nodiscard]] double compute(const Eigen::VectorXd &x) const override;
+public:
+    SquareFunction();
 };
 
 #endif //OPTIMIZER__FUNCTIONS_H
